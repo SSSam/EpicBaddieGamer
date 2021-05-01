315b21738e3f4de8eb120a87650d0ba707deb4de
Sarah <scli@sfu.ca>
2021-05-01 11:47:53 -0700

    Add emoji that dabs at the haters

--- EpicDab/app/src/main/java/com/epicbaddiegamer/epicdab/MainActivity.java
+++ EpicDab/app/src/main/java/com/epicbaddiegamer/epicdab/MainActivity.java
@@ -2,29 +2,41 @@ package com.epicbaddiegamer.epicdab;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static ImageView imgview;
+    private static Button buttonsbm;
+    private int current_image;
+    int[] images={R.drawable.dab_right, R.drawable.dab_left};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        buttonclick();
+
+    }
+    public void buttonclick()
+    {
+        imgview=(ImageView)findViewById(R.id.emoji);
+        buttonsbm=(Button)findViewById(R.id.dabButton);
+        buttonsbm.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        current_image++;
+                        current_image=current_image % images.length;
+                        imgview.setImageResource(images[current_image]);
+                    }
+                }
+
+        );
     }
 
 }
