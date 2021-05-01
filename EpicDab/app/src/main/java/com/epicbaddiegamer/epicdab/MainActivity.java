eeadb0e0f51cce6496b1ca5e7e852602f9df68f8
sjen <sjen@sfu.ca>
2021-05-01 13:19:34 -0700

    Adding 2nd buttom

--- EpicDab/app/src/main/java/com/epicbaddiegamer/epicdab/MainActivity.java
+++ EpicDab/app/src/main/java/com/epicbaddiegamer/epicdab/MainActivity.java
@@ -2,6 +2,7 @@ package com.epicbaddiegamer.epicdab;
 
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,32 +12,61 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private static ImageView imgview;
-    private static Button buttonsbm;
-    private int current_image;
-    int[] images={R.drawable.emoji_right, R.drawable.emoji_left, R.drawable.low_dab_right, R.drawable.low_dab_left, R.drawable.panda_right, R.drawable.panda_left, R.drawable.squidward_right, R.drawable.squidward_left};
+    private  Button left_right_dab;
+    private int current_image_alt=0;
+    private int change_image=0;
+    int[][] images={
+
+            {R.drawable.dab_right, R.drawable.dab_left},
+            {R.drawable.monalisa_left, R.drawable.monalisa_right},
+            {R.drawable.squidward_left, R.drawable.squidward_right},
+            {R.drawable.low_dab_left, R.drawable.low_dab_right}
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonclick();
-
+        dab_buttonclick();
+        change_dabbing_char();
     }
-    public void buttonclick()
+
+
+    public void dab_buttonclick()
     {
         imgview=(ImageView)findViewById(R.id.emoji);
-        buttonsbm=(Button)findViewById(R.id.dabButton);
-        buttonsbm.setOnClickListener(
+        left_right_dab=(Button)findViewById(R.id.dabButton);
+
+        left_right_dab.setOnClickListener(
                 new View.OnClickListener(){
                     @Override
                     public void onClick(View view){
-                        current_image++;
-                        current_image=current_image % images.length;
-                        imgview.setImageResource(images[current_image]);
+                        current_image_alt++;
+                        current_image_alt=current_image_alt % 2;
+                        Log.i("MyApp","currentimg,left right");
+                        imgview.setImageResource(images[change_image][current_image_alt]);
                     }
                 }
-
         );
     }
+    public void change_dabbing_char()
+    {
+        imgview=(ImageView)findViewById(R.id.emoji);
+        Button change_char = (Button) findViewById(R.id.DiffDab);
+
+        change_char.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        change_image++;
+                        change_image=change_image % images.length;
+                        Log.i("App", "change char");
+                        imgview.setImageResource(images[change_image][current_image_alt]);
+                    }
+                }
+        );
+    }
+
 
 }
