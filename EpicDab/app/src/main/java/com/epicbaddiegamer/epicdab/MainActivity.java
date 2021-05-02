e9bdedde394ed72c3d0890327f4747102210c0a6
unknown <jacobhe144@gmail.com>
2021-05-01 19:44:44 -0700

    achievements activity

--- EpicDab/app/src/main/java/com/epicbaddiegamer/epicdab/MainActivity.java
+++ EpicDab/app/src/main/java/com/epicbaddiegamer/epicdab/MainActivity.java
@@ -1,5 +1,6 @@
 package com.epicbaddiegamer.epicdab;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.util.Log;
@@ -14,24 +15,25 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private static ImageView imgview;
-    private  Button left_right_dab;
+    private Button left_right_dab;
+    private Button achievements;
     private int current_image_alt = 0;
     private int change_image = 0;
     private int change = 0;
     private int image_check = 1;
     public int clickCount = 0;
     private TextView countTextBox;
-    int[][] images={
-        {R.drawable.emoji_left, R.drawable.emoji_right},
-        {R.drawable.monalisa_left, R.drawable.monalisa_right},
-        {R.drawable.squidward_left, R.drawable.squidward_right},
-        {R.drawable.low_dab_left, R.drawable.low_dab_right},
-        {R.drawable.fortnite_left, R.drawable.fortnite_right},
-        {R.drawable.panda_left, R.drawable.panda_right},
-        {R.drawable.waluigi_left, R.drawable.waluigi_right},
-        {R.drawable.penguin_left, R.drawable.penguin_right},
-        {R.drawable.shrek_left, R.drawable.shrek_right},
-        {R.drawable.thanos_left, R.drawable.thanos_right}
+    int[][] images = {
+            {R.drawable.emoji_left, R.drawable.emoji_right},
+            {R.drawable.monalisa_left, R.drawable.monalisa_right},
+            {R.drawable.squidward_left, R.drawable.squidward_right},
+            {R.drawable.low_dab_left, R.drawable.low_dab_right},
+            {R.drawable.fortnite_left, R.drawable.fortnite_right},
+            {R.drawable.panda_left, R.drawable.panda_right},
+            {R.drawable.waluigi_left, R.drawable.waluigi_right},
+            {R.drawable.penguin_left, R.drawable.penguin_right},
+            {R.drawable.shrek_left, R.drawable.shrek_right},
+            {R.drawable.thanos_left, R.drawable.thanos_right}
     };
 
     @Override
@@ -41,64 +43,63 @@ public class MainActivity extends AppCompatActivity {
         dab_buttonclick();
         change_dabbing_char();
         change_dabbing_char1();
+        achievements_buttonclick();
 
     }
 
-    public void dab_buttonclick()
-    {
-        imgview=(ImageView)findViewById(R.id.dab);
-        left_right_dab=(Button)findViewById(R.id.dabButton);
+    public void dab_buttonclick() {
+        imgview = (ImageView) findViewById(R.id.dab);
+        left_right_dab = (Button) findViewById(R.id.dabButton);
 
         left_right_dab.setOnClickListener(
-                new View.OnClickListener(){
+                new View.OnClickListener() {
                     @Override
-                    public void onClick(View view){
+                    public void onClick(View view) {
                         clickCount++;
-                        image_check=clickCount/10+1;
-                        countTextBox=findViewById(R.id.textView);
+                        image_check = clickCount / 10 + 1;
+                        countTextBox = findViewById(R.id.textView);
                         String clicks = String.valueOf(clickCount);
                         countTextBox.setText("Cringe Counter: " + clicks);
 
                         current_image_alt++;
-                        current_image_alt=current_image_alt % 2;
-                        Log.i("MyApp","currentimg,left right");
+                        current_image_alt = current_image_alt % 2;
+                        Log.i("MyApp", "currentimg,left right");
                         imgview.setImageResource(images[change][current_image_alt]);
                     }
                 }
         );
     }
 
-    public void change_dabbing_char()
-    {
-        imgview=(ImageView)findViewById(R.id.dab);
+    public void change_dabbing_char() {
+        imgview = (ImageView) findViewById(R.id.dab);
         Button change_char = (Button) findViewById(R.id.DiffDab);
 
         change_char.setOnClickListener(
-                new View.OnClickListener(){
+                new View.OnClickListener() {
                     @Override
-                    public void onClick(View view){
+                    public void onClick(View view) {
 
                         change_image++;
-                        change= (change_image % image_check) % images.length;
+                        change = (change_image % image_check) % images.length;
                         Log.i("App", "change char");
                         imgview.setImageResource(images[change][current_image_alt]);
                     }
                 }
         );
     }
-    public void change_dabbing_char1()
-    {
-        imgview=(ImageView)findViewById(R.id.dab);
+
+    public void change_dabbing_char1() {
+        imgview = (ImageView) findViewById(R.id.dab);
         Button change_char1 = (Button) findViewById(R.id.DiffDab1);
 
         change_char1.setOnClickListener(
-                new View.OnClickListener(){
+                new View.OnClickListener() {
                     @Override
-                    public void onClick(View view){
-                        if (change_image >0) {
+                    public void onClick(View view) {
+                        if (change_image > 0) {
                             change_image--;
                         }
-                        change= (change_image % image_check) % images.length ;
+                        change = (change_image % image_check) % images.length;
                         Log.i("App", "change char");
                         imgview.setImageResource(images[change][current_image_alt]);
                     }
@@ -106,4 +107,21 @@ public class MainActivity extends AppCompatActivity {
         );
     }
 
+
+    public void achievements_buttonclick() {
+
+        Button achievements = (Button) findViewById(R.id.button);
+
+        achievements.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int value = clickCount;
+                        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                        intent.putExtra("value", value);
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
 }
---
+++ EpicDab/app/src/main/java/com/epicbaddiegamer/epicdab/MainActivity2.java
@@ -0,0 +1,81 @@
+package com.epicbaddiegamer.epicdab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    public TextView txtbox1;
+    public TextView txtbox2;
+    public TextView txtbox3;
+    public TextView txtbox4;
+    public TextView txtbox5;
+    public TextView txtbox6;
+    public TextView txtbox7;
+    public TextView txtbox8;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        configureBackButton();
+
+        txtbox1 = findViewById(R.id.textView3);
+        txtbox2 = findViewById(R.id.textView2);
+        txtbox3 = findViewById(R.id.textView4);
+        txtbox4 = findViewById(R.id.textView5);
+        txtbox5 = findViewById(R.id.textView6);
+        txtbox6 = findViewById(R.id.textView7);
+        txtbox7 = findViewById(R.id.textView9);
+        txtbox8 = findViewById(R.id.textView11);
+
+        Bundle bundle = getIntent().getExtras();
+        int value = 0;
+        if (bundle != null) {
+            value = bundle.getInt("value");
+        }
+        if (value > 10) {
+            txtbox1.setTextColor(Color.GREEN);
+        }
+        if (value > 25) {
+            txtbox2.setTextColor(Color.GREEN);
+        }
+        if (value > 100) {
+            txtbox3.setTextColor(Color.GREEN);
+        }
+        if (value > 100) {
+            txtbox4.setTextColor(Color.GREEN);
+        }
+        if (value > 250) {
+            txtbox5.setTextColor(Color.GREEN);
+        }
+        if (value > 1000) {
+            txtbox6.setTextColor(Color.GREEN);
+        }
+        if (value > 2500) {
+            txtbox7.setTextColor(Color.GREEN);
+        }
+        if (value > 10000) {
+            txtbox8.setTextColor(Color.GREEN);
+        }
+    }
+
+    private void configureBackButton() {
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+
+            }
+        });
+
+    }
+}
